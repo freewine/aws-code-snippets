4939bdb3a57954eff21c8fd98db110584979fe26
freewine <freewine@gmail.com>
2022-12-03 18:01:10 +0800

    fix bug: when redirecting to new url, loss querystring in url

--- CloudFront/HostRedirect.js
+++ CloudFront/HostRedirect.js
@@ -1,37 +1,50 @@
+function objectToQueryString(obj) {
+  var params = {};
+
+  Object.keys(obj).sort().forEach(key => {
+      params[key] = obj[key].value;
+  });
+  return querystring.stringify(params);
+}
+var querystring = require('querystring');
 function handler(event) {
   var mobliePrefix = "m-"
   var request = event.request;
   var headers = request.headers;
   
-  var host = request.headers.host.value;
-  var useMoblieUrl = host.startsWith(mobliePrefix)
+  var host = headers.host.value;
+  var useMoblieUrl = host.startsWith(mobliePrefix);
 
-  var desktopDomain = useMoblieUrl ? host.slice(mobliePrefix.length) :  host
-  var mobileDomain = useMoblieUrl ?  host : mobliePrefix + host
+  var desktopDomain = useMoblieUrl ? host.slice(mobliePrefix.length) :  host;
+  var mobileDomain = useMoblieUrl ?  host : mobliePrefix + host;
 
   var uri = request.uri;
   var newHost = host;
   
-  console.log('Original host: ' + JSON.stringify(request));
   if (headers['cloudfront-is-mobile-viewer'] && headers['cloudfront-is-mobile-viewer'].value == "true") {
-      newHost = mobileDomain;
+    newHost = mobileDomain;
   } else {
-      newHost = desktopDomain;
+    newHost = desktopDomain;
   }
-console.log("useMoblieUrl:"+ useMoblieUrl +  "---desktopDomain:" + desktopDomain +  "---mobileDomain:" + mobileDomain+"---newHost:"+ newHost + "---host:"+ host )
-
+  
   if(newHost != host) {
-      var newUrl = `https://${newHost}`;
-      console.log('Request uri set to ' + newUrl);
-      var response = {
-          statusCode: 302,
-          statusDescription: 'OK',
-          headers: {
-              //'host': { value: newHost },
-              'location': { value: newUrl+uri }
-          }
-      }
-      return response;
+    var redirectUrl = "";
+
+    if (Object.keys(request.querystring).length) {
+      redirectUrl = `https://${newHost}${uri}?${objectToQueryString(request.querystring)}`;
+    }
+    else {
+      redirectUrl = `https://${newHost}${uri}`;
+    }
+
+    var response = {
+        statusCode: 302,
+        statusDescription: 'Found',
+        headers: {
+            'location': { value: redirectUrl }
+        }
+    }
+    return response;
   }
 
   return request;
